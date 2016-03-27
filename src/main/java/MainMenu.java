076dfa257aaa53d64da6cc58497d1dea0eace696
mmr63 <mmr63@pitt.edu>
2016-03-27 14:25:34 -0400

    Added files via upload

---
+++ src/main/java/MainMenu.java
@@ -0,0 +1,13 @@
+package edu.pitt.cs.cs1635.mmr63.wishlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
